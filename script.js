22a72be1a3f4eb4fa840b48367cb01e0578a4677
jayamjk <125721902+jayamjk@users.noreply.github.com>
2026-01-03 01:26:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("menuToggle");
+  const nav = document.getElementById("navMenu");
+
+  toggle.addEventListener("click", () => {
+    nav.style.display = nav.style.display === "flex" ? "none" : "flex";
+  });
+});
